c041dda408b821ce22c2df30bbbc998b89888fa4
wegrowupsolutions <contato@wegrowup.com.br>
2025-09-01 14:33:33 -0300

    build: adiciona pasta dist para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: base: '/afiliado/',
+  base: '/afiliado/',
 
   plugins: [react()],
   build: {
